08be99b9b88117f04729ab94659911016f48ce58
Luuk Glorie <LuukGlorie@users.noreply.github.com>
2024-12-04 20:52:01 +0000

    day 1 complete

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,40 @@
-﻿namespace AdventOfCode;
+﻿using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly IReadOnlyList<string> _input;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadLines(InputFilePath).ToList();
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override async ValueTask<string> Solve_1() 
+    {
+        var values = _input
+            .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        
+        var firstColumn = values.Select(x => x.First()).Order();
+        var secondColumn = values.Select(x => x.Last()).Order();
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        var result = firstColumn.Zip(secondColumn, (x, y) => Math.Abs(x - y)).Sum();
+        return result.ToString();
+    }
+
+    public override async ValueTask<string> Solve_2() 
+    {
+        var values = _input
+            .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        
+        var firstColumn = values.Select(x => x.First());
+        var secondColumn = values.Select(x => x.Last())
+            .GroupBy(x => x, (x, ys) => (x, ys.Count()))
+            .ToDictionary(x => x.x, x => x.Item2);
+
+        var result = firstColumn.Select(x => x * (secondColumn.TryGetValue(x, out var y) ? y : 0)).Sum();
+        return result.ToString();
+    }
 }
---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace AdventOfCode;
+
+public class Day02 : BaseDay
+{
+    private readonly IReadOnlyList<string> _input;
+
+    public Day02()
+    {
+        _input = File.ReadLines(InputFilePath).ToList();
+    }
+
+    public override async ValueTask<string> Solve_1() 
+    {
+        
+        
+    }
+
+    public override async ValueTask<string> Solve_2() 
+    {
+        return "";
+    }
+}
